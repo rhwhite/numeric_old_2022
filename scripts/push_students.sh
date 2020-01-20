37388848531cdc6222cd0cc99836df0714699e22
Phil Austin <paustin@eos.ubc.ca>
2020-01-20 13:34:09 -0800

    checkpoint

--- scripts/push_students.sh
+++ scripts/push_students.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
+#!/bin/bash
 #
 # update numeric_students repo master branch
 # with contents of students folder
 #
-ghp-import  -p -b downloads -r students students 
+ghp-import -p -b downloads -r students students 
